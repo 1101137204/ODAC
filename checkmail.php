456e1cdecaaf4131c67a045d05a751a60a123add
ALEX <ALEX@172.26.14.0>
2015-12-17 15:32:47 +0800

    v18.0

--- checkmail.php
+++ checkmail.php
@@ -7,11 +7,11 @@ try {
     $ups = base64_encode(htmlspecialchars(@$_GET['ups']));
     $name = htmlspecialchars(@$_GET['name']);
 
-    if (!empty($uid) && !empty($ups) && !empty($name)) {
+    if (!empty($uid) && !empty(base64_decode($ups)) && !empty($name)) {
         $db = new PDO($dsn, $db_user, $db_password);
         $sql = "SELECT Member_ID, Identity, Name, Phone, Email, Password, Active 
                 FROM member 
-                WHERE Email='" . $uid . "' AND Password='" . base64_decode($ups) . "' AND Name='" . $name . "'";
+                WHERE Email='" . $uid . "' AND Password='" . $ups . "' AND Name='" . $name . "'";
         $stmt = $db->query($sql);
         $row = $stmt->fetch();
         if ($row['Active']) {
@@ -19,7 +19,7 @@ try {
             header("refresh:1; url=index.php");
         } else if (!empty($row)) {
             $sql2 = "UPDATE member SET Active = '1' 
-                    WHERE Email='" . $uid . "' AND Password='" . base64_decode($ups) . "'AND Name='" . $name . "'";
+                    WHERE Email='" . $uid . "' AND Password='" . $ups . "'AND Name='" . $name . "'";
             $count = $db->exec($sql2);
             echo '帳號認證成功!!';
             header("refresh:1; url=index.php");
