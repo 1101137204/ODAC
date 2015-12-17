4c8e98c965f1108f2c9abcd0098ab2eb92238cdc
ALEX <ALEX@172.26.14.0>
2015-12-17 14:45:28 +0800

    v11.0

--- connectdB.php
+++ connectdB.php
@@ -4,7 +4,8 @@ try {
     $db_host = 'www.db4free.net:3306';
     $db_name = 'odac';
     $db_user = 'odac2015';
-    $db_password = 'odac2015';
+    $str='b2RhYzIwMTU=';
+    $db_password = base64_decode($str);
     $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
 } catch (Exception $exc) {
     echo $exc->getMessage();
